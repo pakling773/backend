40e8c0ea4e0bccd06c42f4a7c3de306e8fce13a3
pakling773 <pakling773@gmail.com>
2024-05-27 10:54:59 +0545

    auth required

--- src/middleware/authMiddleware.ts
+++ src/middleware/authMiddleware.ts
@@ -3,16 +3,16 @@ import "dotenv/config";
 
 function verifyToken(req, res, next) {
   next();
-  // const token = req.header("Authorization");
-  // if (!token) return res.status(401).json({ error: "Access denied..." });
-  // try {
-  //   const token = req.headers.authorization.split(" ")[1];
-  //   const decoded = jwt.verify(token, process.env.API_SECRET);
-  //   req.userId = decoded.userId;
-  //   next();
-  // } catch (error) {
-  //   res.status(401).json({ error: "Invalid token", e: error });
-  // }
+  const token = req.header("Authorization");
+  if (!token) return res.status(401).json({ error: "Access denied..." });
+  try {
+    const token = req.headers.authorization.split(" ")[1];
+    const decoded = jwt.verify(token, process.env.API_SECRET);
+    req.userId = decoded.userId;
+    next();
+  } catch (error) {
+    res.status(401).json({ error: "Invalid token", e: error });
+  }
 }
 
 export default verifyToken;
